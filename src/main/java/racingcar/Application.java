371e7f658063728fabcac4b748cfd27394ccea70
hyojun <hjlim45678@gmail.com>
2024-08-21 15:45:21 +0900

    fix(Application): ã안쓰는코드 삭제

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -4,9 +4,5 @@ public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
 
-        System.out.println("시작하자31gg1");
-        while (true){
-            if(true){
-        }
     }
 }
