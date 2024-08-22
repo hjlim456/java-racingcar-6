0203f8133dd0087f5e202e3bab95bafc11cd824b
hyojun <hjlim45678@gmail.com>
2024-08-22 11:27:38 +0900

    refactor: 이름 입력 메시지 문제 예시와 똑같이 변경

--- src/main/java/racingcar/message/ViewMessage.java
+++ src/main/java/racingcar/message/ViewMessage.java
@@ -1,7 +1,7 @@
 package racingcar.message;
 
 public interface ViewMessage {
-    String INPUT_CAR_NAMES = "경주할 자동차들의 이름을 입력하세요.(쉼표(,) 기준으로 이름구분)";
+    String INPUT_CAR_NAMES = "경주할 자동차들의 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
     String INPUT_TRIAL_COUNT = "시도할 회수입력하세요";
     String GAME_RESULT = "실행 결과";
 }
