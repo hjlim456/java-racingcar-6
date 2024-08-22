bb663095a276c3090a73796f9e1c7c0d1848a774
hyojun <hjlim45678@gmail.com>
2024-08-22 09:58:49 +0900

    feat : 시도횟수 음수일떄의 예외처리 구현

--- src/main/java/racingcar/ErrorMessage.java
+++ src/main/java/racingcar/ErrorMessage.java
@@ -5,4 +5,6 @@ public interface ErrorMessage {
     String INPUT_NOT_INTEGER = "숫자만 입력 할 수 있습니다.";
     String INPUT_ZERO = " 횟수는 1번이상 입력가능합니다. ";
     String EXCESS_NAME_LENGTH_LIMIT = "자동차 이름은 5자 이하여야 합니다.";
+    String INVALID_TRIAL_COUNT = "시도횟수는 음수일 수 없습니다.";
+
 }
--- src/main/java/racingcar/GameData.java
+++ src/main/java/racingcar/GameData.java
@@ -3,4 +3,13 @@ package racingcar;
 import java.util.List;
 
 public record GameData(List<Car> carList, int trialCount) {
+    public GameData{
+        validTrialCount(trialCount);
+    }
+
+    private void validTrialCount(int trialCount) {
+        if (trialCount < 0) {
+            throw new IllegalArgumentException(ErrorMessage.INVALID_TRIAL_COUNT);
+        }
+    }
 }
