6ee0dbc0e5103d8c39f940e9d2ce8a4afd36afd0
hyojun <hjlim45678@gmail.com>
2024-08-23 12:40:21 +0900

    test: Car 이름입력 실패 테스트 구현

--- src/test/java/racingcar/domain/CarTest.java
+++ src/test/java/racingcar/domain/CarTest.java
@@ -4,16 +4,26 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
+import racingcar.message.ErrorMessage;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.*;
 
 class CarTest {
-
 
     @ParameterizedTest
     @ValueSource(strings = {"pobi", "aaaa", "A2", "@2"})
     void nameSuccess(String name) {
         Car car = new Car(name);
-        Assertions.assertThat(car.getName()).isEqualTo(name);
+        assertThat(car.getName()).isEqualTo(name);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"Morethan4", "        ",""})
+    void nameFail(String name) {
+        assertThatThrownBy(()->{
+            new Car(name);
+        }).isInstanceOf(IllegalArgumentException.class);
     }
 }
